fb4113b89fba83e7531e06082ac5f6f7a57cb47f
HashiCorp Training <training@hashicorp.com>
2017-01-27 17:56:20 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-7e50c21a
+#
+# Your security group ID is:
+#
+#     sg-29ef374e
+#
+# Your AMI ID is:
+#
+#     ami-30217250
+#
+# Your Identity is:
+#
+#     autodesk-chicken
+#
+
+variable "aws_access_key" {
+  type = "string"
+}
+
+variable "aws_secret_key" {
+  type = "string"
+}
+
+variable "aws_region" {
+  type    = "string"
+  default = "us-west-1"
+}
+
+variable "instance_cnt" {
+  default = "2"
+}
+
+variable "command" {
+  default = "echo 'Hello Ben Szutu'"
+}
+
+/*
+module "example-module" {
+  source = "./example-module"
+  command = "echo Hello Ben Szutu"
+}
+*/
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-30217250"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-7e50c21a"
+  vpc_security_group_ids = ["sg-29ef374e"]
+  count = "2"
+  tags {
+    Identity = "autodesk-chicken"
+    server   = "web"
+    product  = "test"
+    name = "web ${count.index+1}/${var.instance_cnt}"
+  }
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
